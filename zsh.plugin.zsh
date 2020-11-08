7194812c78dd44fa3868bce1a421f12c712a0222
EMayej <EMayej@gmail.com>
2020-11-08 14:04:15 +0800

    feat: add zsh-hook of fasd
    no completion and aliases

--- zsh.plugin.zsh
+++ zsh.plugin.zsh
@@ -1,4 +1,11 @@
 autoload -Uz take
 
+fasd_cache="${ZSH_CACHE_DIR}/fasd-init-cache"
+if [[ "$commands[fasd]" -nt "$fasd_cache" || ! -s "$fasd_cache" ]]; then
+    fasd --init zsh-hook >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
+
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude={.git,build}'
 export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --border"
