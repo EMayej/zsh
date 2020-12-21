7f5243da41bce64e17c44907ef5d77719c1a7fda
EMayej <EMayej@gmail.com>
2020-12-21 16:04:07 +0800

    refactor: aliases managed with olets/zsh-abbr

--- zsh.plugin.zsh
+++ zsh.plugin.zsh
@@ -6,8 +6,6 @@ export EDITOR=emacsclient
 
 autoload -Uz take
 
-alias cat=bat
-
 fasd_cache="${ZSH_CACHE_DIR}/fasd-init-cache"
 if [[ "$commands[fasd]" -nt "$fasd_cache" || ! -s "$fasd_cache" ]]; then
     fasd --init zsh-hook >| "$fasd_cache"
@@ -33,20 +31,3 @@ autoload -Uz pet-save
 autoload -Uz pet-load
 zle -N pet-load
 bindkey '^l' pet-load
-
-# git aliases
-alias gd="git diff"
-alias gf="git fetch --all"
-alias gr='cd $(git rev-parse --show-toplevel)'
-alias gst="git status -s"
-alias gt="git log --graph --format='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen%an%Creset %C(bold blue)%cr%Creset' --all"
-alias gu="git pull --rebase --autostash"
-
-# https://github.com/DarrinTisdale/zsh-aliases-exa
-alias ls='exa'
-alias l='exa -lbF --git'                    # list, size, type, git
-alias ll='exa -lbGF --git'                  # long list
-alias llm='exa -lbGF --git --sort=modified' # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale' # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-alias lt='exa --tree --level=2' # tree
